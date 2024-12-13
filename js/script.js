582560c97b4f553de7bb0272944352763bac09b4
Swetlana Warkentin <60183456+smamvision@users.noreply.github.com>
2024-12-13 13:02:38 +0100

    tollbar

--- js/script.js
+++ js/script.js
@@ -198,30 +198,3 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
-
-//========================================
-//Hide Toolbar on Scroll iOS
-//========================================
-let lastScrollPosition = window.scrollY;
-const navbar = document.querySelector('.navbar');
-const threshold = 100; // Mindestscrollabstand bevor die Navbar sich versteckt
-
-window.addEventListener('scroll', () => {
-	const currentScrollPosition = window.scrollY;
-	
-	// Prüfen ob genug gescrollt wurde
-	if (Math.abs(lastScrollPosition - currentScrollPosition) <= threshold) {
-		return;
-	}
-
-	// Nach unten scrollen versteckt die Navbar
-	if (currentScrollPosition > lastScrollPosition) {
-		navbar.classList.add('hidden');
-	} 
-	// Nach oben scrollen zeigt die Navbar
-	else {
-		navbar.classList.remove('hidden');
-	}
-
-	lastScrollPosition = currentScrollPosition;
-});
