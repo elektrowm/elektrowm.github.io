a5abd94e6ed433ec96ab20f78460e9cd162b27ce
Swetlana Warkentin <60183456+smamvision@users.noreply.github.com>
2024-12-28 08:54:42 +0100

    text

--- js/form.js
+++ js/form.js
@@ -71,8 +71,8 @@ const formItemsWithRegex = [
     [surnameCust, /^[a-zA-ZäöüßÄÖÜ\s-]{2,}$/],
     [emailCust, /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/],
     [phoneCust, /^[\d\s+()-]{5,}$/],
-    [subjectCust, /^[\wäöüßÄÖÜ\s-]{2,}$/],
-    [messageCust, /^[\wäöüßÄÖÜ\s.,!?-]{2,}$/],
+    [subjectCust, /^.{2,}$/s],
+    [messageCust, /^.{2,}$/s],
     [consentCust, / /] 
 ];
 
